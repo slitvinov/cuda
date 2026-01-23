fce6fdb35362db9f3acd74ea26783635acd39588
Sergey Litvinov <slitvinov@seas.harvard.edu>
2026-01-23 16:35:40 -0500

    up

--- hw.cu
+++ hw.cu
@@ -15,13 +15,12 @@ __device__ __forceinline__ unsigned int sm_id() {
 __global__ void kernel() {
   unsigned int lane = lane_id();
   unsigned int sm = sm_id();
-
   printf("threadIdx.x=%d lane=%u smid=%u\n", threadIdx.x, lane, sm);
 }
 
 int main() {
   int gridSize = 1;
-  int blockSize = 64;
+  int blockSize = 256;
   kernel<<<gridSize, blockSize>>>();
   cudaDeviceSynchronize();
 }
