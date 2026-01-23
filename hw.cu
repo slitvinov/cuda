31095f4a81474acc3d522c8c7d67b0944b6f0301
Sergey Litvinov <slitvinov@seas.harvard.edu>
2026-01-23 16:31:13 -0500

    sm

--- hw.cu
+++ hw.cu
@@ -6,15 +6,21 @@ __device__ __forceinline__ unsigned int lane_id() {
   return id;
 }
 
+__device__ __forceinline__ unsigned int sm_id() {
+  unsigned int id;
+  asm("mov.u32 %0, %smid;" : "=r"(id));
+  return id;
+}
+
 __global__ void kernel() {
   unsigned int lane = lane_id();
-  printf("threadIdx.x=%d lane=%u\n",
-         threadIdx.x, lane);
+  unsigned int sm = sm_id();
+
+  printf("threadIdx.x=%d lane=%u smid=%u\n", threadIdx.x, lane, sm);
 }
 
 int main() {
   int gridSize = 1;
   int blockSize = 64;
   kernel<<<gridSize, blockSize>>>();
-  cudaDeviceSynchronize();
 }
