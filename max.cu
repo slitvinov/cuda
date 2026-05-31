049d40a3583ca8f1ed49ea2cfe8aeb471a0edb99
Sergey Litvinov <slitvinov@gmial.com>
2026-05-31 12:39:37 +0200

    add max

---
+++ max.cu
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <cuda/cmath>
+#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
+#include <numeric>
+#include <random>
+#include <cassert>
+enum {n = 32, th = 32};
+__global__ void f(int *a) {
+  int tid, s;
+  __shared__ int b[n], c[n];
+  tid = blockIdx.x * blockDim.x + threadIdx.x;
+  b[tid] = a[tid];
+  c[tid] = tid;
+  #pragma unroll
+  for (s = 1; s < n; s <<= 1) {
+    __syncwarp();
+    if (tid + s < n) {
+      if (b[tid] < b[tid + s]) {
+        b[tid] = b[tid + s];
+        c[tid] = c[tid + s];
+      }
+    }
+  }
+  if (tid == 0) {
+    a[0] = b[0];
+    a[1] = c[0];
+  }
+}
+int main() {
+  int bl, i, *a;
+  cudaDeviceProp prop;
+  cudaGetDeviceProperties(&prop, 0);
+  assert(prop.warpSize == th);
+  assert(n == th);
+  cudaMallocManaged(&a, n * sizeof *a);
+  std::mt19937 rng(0);
+  std::iota(a, a + n, 0);
+  std::shuffle(a, a + n, rng);
+  for (i = 0; i < n; i++)
+    printf("%02d ", a[i]);
+  printf("\n");
+  bl = (n + th - 1) / th;
+  f<<<bl, th>>>(a);
+  cudaDeviceSynchronize();
+  assert(a[0] == n - 1);
+  for (i = 0; i < n; i++)
+    printf(i == a[1] ? "^^ " : "   ");
+  cudaFree(a);
+  return 0;
+}
