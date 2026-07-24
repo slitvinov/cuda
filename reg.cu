dc84aae687f6b74fbb901e891bfdda770153b1ee
Sergey Litvinov <slitvinov@gmial.com>
2026-07-24 11:37:29 +0200

    up

--- reg.cu
+++ reg.cu
@@ -1,17 +1,18 @@
-#include <stdint.h>
-#include <reg.h>
-#include <stdio.h>
 #include <inttypes.h>
-
+#include <reg.h>
+#include <stdint.h>
+#include <stdio.h>
 
 __global__ void kernel() {
-  uint32_t laneid, smid;
+  uint32_t laneid, smid, warpid;
   uint64_t clock64;
   reg_laneid(&laneid);
   reg_smid(&smid);
   reg_clock64(&clock64);
-  printf("%5" PRIu32 " %3" PRIu32 " %5" PRIu32 " %8" PRIu64 "\n",
-       threadIdx.x, laneid, smid, clock64);
+  reg_warpid(&warpid);
+
+  printf("%5" PRIu32 " %3" PRIu32 " %3" PRIu32 " %5" PRIu32 " %8" PRIu64 "\n",
+         threadIdx.x, laneid, warpid, smid, clock64);
 }
 
 int main(int argc, char **argv) {
--- reg.h
+++ reg.h
@@ -3,34 +3,90 @@
   V3(ntid)                                                                     \
   V3(ctaid)                                                                    \
   V3(nctaid)                                                                   \
-  R32(laneid)                                                                  \
-  R32(warpid)                                                                  \
-  R32(nwarpid)                                                                 \
-  R32(smid)                                                                    \
-  R32(nsmid)                                                                   \
-  MASK(lanemask_eq)                                                            \
-  MASK(lanemask_lt)                                                            \
-  MASK(lanemask_le)                                                            \
-  MASK(lanemask_ge)                                                            \
-  MASK(lanemask_gt)                                                            \
-  R32(dynamic_smem_size)                                                       \
-  R32(total_smem_size)                                                         \
-  R32(clock)                                                                   \
-  CNT(clock64)                                                                 \
-  R64(gridid)                                                                  \
-  TNS(globaltimer)
+  V3(clusterid)                                                                \
+  V3(nclusterid)                                                               \
+  V3(cluster_ctaid)                                                            \
+  V3(cluster_nctaid)                                                           \
+  U32(laneid)                                                                  \
+  U32(warpid)                                                                  \
+  U32(nwarpid)                                                                 \
+  U32(smid)                                                                    \
+  U32(nsmid)                                                                   \
+  U32(cluster_ctarank)                                                         \
+  U32(cluster_nctarank)                                                        \
+  U32(lanemask_eq)                                                             \
+  U32(lanemask_le)                                                             \
+  U32(lanemask_lt)                                                             \
+  U32(lanemask_ge)                                                             \
+  U32(lanemask_gt)                                                             \
+  U32(clock)                                                                   \
+  U32(clock_hi)                                                                \
+  U64(clock64)                                                                 \
+  U64(globaltimer)                                                             \
+  U32(globaltimer_lo)                                                          \
+  U32(globaltimer_hi)                                                          \
+  U64(gridid)                                                                  \
+  U32(total_smem_size)                                                         \
+  U32(aggr_smem_size)                                                          \
+  U32(dynamic_smem_size)                                                       \
+  U64(current_graph_exec)                                                      \
+  U32(pm0)                                                                     \
+  U32(pm1)                                                                     \
+  U32(pm2)                                                                     \
+  U32(pm3)                                                                     \
+  U32(pm4)                                                                     \
+  U32(pm5)                                                                     \
+  U32(pm6)                                                                     \
+  U32(pm7)                                                                     \
+  U64(pm0_64)                                                                  \
+  U64(pm1_64)                                                                  \
+  U64(pm2_64)                                                                  \
+  U64(pm3_64)                                                                  \
+  U64(pm4_64)                                                                  \
+  U64(pm5_64)                                                                  \
+  U64(pm6_64)                                                                  \
+  U64(pm7_64)                                                                  \
+  U32(envreg0)                                                                 \
+  U32(envreg1)                                                                 \
+  U32(envreg2)                                                                 \
+  U32(envreg3)                                                                 \
+  U32(envreg4)                                                                 \
+  U32(envreg5)                                                                 \
+  U32(envreg6)                                                                 \
+  U32(envreg7)                                                                 \
+  U32(envreg8)                                                                 \
+  U32(envreg9)                                                                 \
+  U32(envreg10)                                                                \
+  U32(envreg11)                                                                \
+  U32(envreg12)                                                                \
+  U32(envreg13)                                                                \
+  U32(envreg14)                                                                \
+  U32(envreg15)                                                                \
+  U32(envreg16)                                                                \
+  U32(envreg17)                                                                \
+  U32(envreg18)                                                                \
+  U32(envreg19)                                                                \
+  U32(envreg20)                                                                \
+  U32(envreg21)                                                                \
+  U32(envreg22)                                                                \
+  U32(envreg23)                                                                \
+  U32(envreg24)                                                                \
+  U32(envreg25)                                                                \
+  U32(envreg26)                                                                \
+  U32(envreg27)                                                                \
+  U32(envreg28)                                                                \
+  U32(envreg29)                                                                \
+  U32(envreg30)                                                                \
+  U32(envreg31)
 
-#define R32(n)                                                                 \
+#define U32(n)                                                                 \
   static __device__ __forceinline__ void reg_##n(uint32_t *p) {                \
     asm volatile("mov.u32 %0, %%" #n ";" : "=r"(*p));                          \
   }
-#define R64(n)                                                                 \
+#define U64(n)                                                                 \
   static __device__ __forceinline__ void reg_##n(uint64_t *p) {                \
     asm volatile("mov.u64 %0, %%" #n ";" : "=l"(*p));                          \
   }
-#define MASK(n) R32(n)
-#define CNT(n) R64(n)
-#define TNS(n) R64(n)
 #define V3(n)                                                                  \
   static __device__ __forceinline__ void reg_##n(uint32_t p[3]) {              \
     asm volatile("mov.u32 %0, %%" #n ".x;" : "=r"(p[0]));                      \
@@ -38,9 +94,14 @@
     asm volatile("mov.u32 %0, %%" #n ".z;" : "=r"(p[2]));                      \
   }
 SREGS
-#undef R32
-#undef R64
+#undef U32
+#undef U64
 #undef V3
-#undef MASK
-#undef CNT
-#undef TNS
+
+static __device__ __forceinline__ void reg_is_explicit_cluster(uint32_t *p) {
+  asm volatile("{ .reg .pred %%q;                      \n\t"
+               "  mov.pred %%q, %%is_explicit_cluster; \n\t"
+               "  selp.u32 %0, 1, 0, %%q;              \n\t"
+               "}"
+               : "=r"(*p));
+}
