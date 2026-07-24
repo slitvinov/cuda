98432dc07b9d5a245019da7598a62925b8f67d6b
Sergey Litvinov <slitvinov@gmial.com>
2026-07-24 20:25:51 +0200

    time

--- max.cu
+++ max.cu
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <cuda/cmath>
+#include <inttypes.h>
+#include <reg.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
 #include <algorithm>
 #include <numeric>
 #include <random>
 #include <cassert>
-enum {n = 32, th = 32};
-__global__ void f(int *a) {
+enum {n = 32, th = 32, iters = 100};
+__global__ void f(int *a, int64_t *t) {
   int tid, s;
+  uint64_t start, end;
   __shared__ int b[n], c[n];
   tid = blockIdx.x * blockDim.x + threadIdx.x;
+  reg_clock64(&start);
   b[tid] = a[tid];
   c[tid] = tid;
   #pragma unroll
@@ -23,33 +28,43 @@ __global__ void f(int *a) {
       }
     }
   }
+  __syncwarp();
+  reg_clock64(&end);
   if (tid == 0) {
     a[0] = b[0];
     a[1] = c[0];
+    t[0] = end - start;
   }
 }
 int main(int argc, char **argv) {
-  int bl, i, j, *a;
+  int bl, i, j, *a, h[n];
+  int64_t *t, ht[iters];
   cudaDeviceProp prop;
   cudaGetDeviceProperties(&prop, 0);
   assert(prop.warpSize == th);
   assert(n == th);
-  cudaMallocManaged(&a, n * sizeof *a);
+  cudaMalloc(&a, n * sizeof *a);
+  cudaMalloc(&t, iters * sizeof *t);
   std::mt19937 rng(argv[1] ? atoi(argv[1]) : 0);
-  for (j = 0; j < 10; j++) {
-    std::iota(a, a + n, 0);
-    std::shuffle(a, a + n, rng);
+  for (j = 0; j < iters; j++) {
+    std::iota(h, h + n, 0);
+    std::shuffle(h, h + n, rng);
     for (i = 0; i < n; i++)
-      printf("%02d ", a[i]);
+      printf("%02d ", h[i]);
     printf("\n");
     bl = (n + th - 1) / th;
-    f<<<bl, th>>>(a);
-    cudaDeviceSynchronize();
-    assert(a[0] == n - 1);
+    cudaMemcpy(a, h, n * sizeof *a, cudaMemcpyHostToDevice);
+    f<<<bl, th>>>(a, t + j);
+    cudaMemcpy(h, a, 2 * sizeof *a, cudaMemcpyDeviceToHost);
+    assert(h[0] == n - 1);
     for (i = 0; i < n; i++)
-      printf(i == a[1] ? "^^ " : "   ");
+      printf(i == h[1] ? "^^ " : "   ");
     printf("\n");
   }
+  cudaMemcpy(ht, t, iters * sizeof *t, cudaMemcpyDeviceToHost);
+  for (j = 0; j < iters; j++)
+    printf("%5d %8" PRIu64 "\n", j, ht[j]);
+  cudaFree(t);
   cudaFree(a);
   return 0;
 }
