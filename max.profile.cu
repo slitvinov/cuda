c271c609d55d05487aa166b22fb92990cede1f91
Sergey Litvinov <slitvinov@gmial.com>
2026-07-25 02:09:39 +0200

    add globaltime

--- max.profile.cu
+++ max.profile.cu
@@ -3,20 +3,29 @@
 #include <reg.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <algorithm>
 #include <numeric>
 #include <random>
 #include <cassert>
 enum {n = 32, th = 32, warm = 100, iters = 1000000, NT = 10};
-__global__ void f(int *a, int64_t *t) {
-  uint64_t ts[NT];
+struct Rec {
+  uint32_t smid, warpid;
+  uint64_t gt, ts[NT];
+};
+__global__ void f(int *a, Rec *r) {
+  uint64_t ts[NT], gt;
   uint32_t smid, warpid;
   int k = 0, tid, s;
   __shared__ int b[n], c[n];
 #define TS() reg_clock64(&ts[k++])
   reg_smid(&smid);
   reg_warpid(&warpid);
+  reg_globaltimer(&gt);
+#pragma unroll
+  for (int i = 0; i < NT; i++)
+    ts[i] = ~0ULL;
   TS();
   tid = blockIdx.x * blockDim.x + threadIdx.x;
   TS();
@@ -24,7 +33,7 @@ __global__ void f(int *a, int64_t *t) {
   TS();
   c[tid] = tid;
   TS();
-  #pragma unroll
+#pragma unroll
   for (s = n / 2; s > 0; s >>= 1) {
     __syncwarp();
     if (tid < s) {
@@ -39,28 +48,28 @@ __global__ void f(int *a, int64_t *t) {
     a[0] = b[0];
     a[1] = c[0];
   }
-  t[tid * NT] = smid;
-  t[tid * NT + 1] = warpid;
-  for (int i = 0; i < NT - 2; i++)
-    if (i < k)
-      t[tid * NT + i + 2] = ts[i];
+  r[tid].smid = smid;
+  r[tid].warpid = warpid;
+  r[tid].gt = gt;
+#pragma unroll
+  for (int i = 0; i < NT; i++)
+    r[tid].ts[i] = ts[i];
 }
 int main(int argc, char **argv) {
   int bl, *a, *a2, h[n];
   size_t i, j;
-  int64_t *t, *tjunk;
-  int64_t ht[th * NT];
+  Rec *r, *rjunk, hr[th];
   cudaDeviceProp prop;
   cudaGetDeviceProperties(&prop, 0);
   assert(prop.warpSize == th);
   assert(n == th);
   cudaMalloc(&a, n * sizeof *a);
   cudaMalloc(&a2, n * sizeof *a2);
-  cudaMalloc(&tjunk, th * NT * sizeof *tjunk);
+  cudaMalloc(&rjunk, th * sizeof *rjunk);
   size_t flushbytes = 2 * (size_t)prop.l2CacheSize;
   char *flush;
   cudaMalloc(&flush, flushbytes);
-  cudaMalloc(&t, th * NT * sizeof *t);
+  cudaMalloc(&r, th * sizeof *r);
   std::mt19937 rng(argv[1] ? atoi(argv[1]) : 0);
   bl = (n + th - 1) / th;
   for (j = 0; j < warm + iters; j++) {
@@ -68,25 +77,24 @@ int main(int argc, char **argv) {
     std::shuffle(h, h + n, rng);
     cudaMemcpy(a, h, n * sizeof *a, cudaMemcpyHostToDevice);
     cudaMemset(flush, j, flushbytes);
-    cudaMemset(t, 0xff, th * NT * sizeof *t);
-    f<<<bl, th>>>(a2, tjunk);
-    f<<<bl, th>>>(a, t);
-    cudaMemcpy(ht, t, th * NT * sizeof *t, cudaMemcpyDeviceToHost);
+    f<<<bl, th>>>(a2, rjunk);
+    f<<<bl, th>>>(a, r);
+    cudaMemcpy(hr, r, th * sizeof *r, cudaMemcpyDeviceToHost);
     if (j < warm)
       continue;
     for (int lane = 1; lane < th; lane++)
-      for (int slot = 0; slot < NT; slot++)
-        assert(ht[lane * NT + slot] == ht[slot]);
-    printf("%" PRId64 " %" PRId64 " ", ht[0], ht[1]);
-    for (i = 3; i < NT; i++)
-      if (ht[i] != -1)
-        printf("%" PRId64 " ", ht[i] - ht[2]);
+      assert(memcmp(&hr[lane], &hr[0], sizeof(Rec)) == 0);
+    printf("%" PRIu32 " %" PRIu32 " %" PRIu64 " ", hr[0].smid, hr[0].warpid,
+           hr[0].gt);
+    for (i = 1; i < NT; i++)
+      if (hr[0].ts[i] != ~0ULL)
+        printf("%" PRIu64 " ", hr[0].ts[i] - hr[0].ts[0]);
     printf("\n");
   }
   cudaFree(flush);
-  cudaFree(tjunk);
+  cudaFree(rjunk);
   cudaFree(a2);
-  cudaFree(t);
+  cudaFree(r);
   cudaFree(a);
   return 0;
 }
