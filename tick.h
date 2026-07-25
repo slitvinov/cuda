6042d136aab8bd64dc424d5190975a87411d4032
Sergey Litvinov <slitvinov@gmial.com>
2026-07-25 18:17:53 +0200

    up

--- tick.h
+++ tick.h
@@ -7,24 +7,24 @@
   U32(globaltimer_hi)
 
 #define U32(n)                                                                 \
-  static __device__ __forceinline__ void tick_##n(uint64_t fake,               \
+  static __device__ __forceinline__ void tick_##n(uint32_t fake,               \
                                                    uint32_t *p) {              \
     asm volatile("{ .reg .pred %%q;              \n\t"                          \
-                 "  setp.ge.u64 %%q, %1, 0;      \n\t"                          \
+                 "  setp.ge.u32 %%q, %1, 0;      \n\t"                          \
                  "  @%%q mov.u32 %0, %%" #n ";   \n\t"                          \
                  "}"                                                           \
                  : "=r"(*p)                                                    \
-                 : "l"(fake));                                                 \
+                 : "r"(fake));                                                 \
   }
 #define U64(n)                                                                 \
-  static __device__ __forceinline__ void tick_##n(uint64_t fake,               \
+  static __device__ __forceinline__ void tick_##n(uint32_t fake,               \
                                                    uint64_t *p) {              \
     asm volatile("{ .reg .pred %%q;              \n\t"                          \
-                 "  setp.ge.u64 %%q, %1, 0;      \n\t"                          \
+                 "  setp.ge.u32 %%q, %1, 0;      \n\t"                          \
                  "  @%%q mov.u64 %0, %%" #n ";   \n\t"                          \
                  "}"                                                           \
                  : "=l"(*p)                                                    \
-                 : "l"(fake));                                                 \
+                 : "r"(fake));                                                 \
   }
 TICKS
 #undef U32
