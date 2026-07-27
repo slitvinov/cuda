343ab3c11408059f7d972be8bb427d69e3d39917
Sergey Litvinov <slitvinov@gmial.com>
2026-07-27 13:58:46 +0200

    pchase.sh

---
+++ pchase.sh
@@ -0,0 +1,16 @@
+set -eu
+mkdir -p data2
+s=0
+while test $s -lt 132
+do ./hal.sh nvcc -I. -arch native -run pchase.cu -run-args -s,$s,-n,1024,-i,10000,-w,16,-b,x.$s
+   case $? in
+       0 ) ;;
+       * ) exit $? ;;
+   esac
+   ./hal.sh -f x.$s.raw x.$s.meta data2/
+   case $? in
+       0 ) ;;
+       * ) exit $? ;;
+   esac
+   s=$((s+1))
+done
