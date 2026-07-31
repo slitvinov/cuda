6fed4ce3853dc52e21a582adc2eb130884fc6d65
Sergey Litvinov <slitvinov@gmial.com>
2026-07-31 06:47:22 -0400

    fix info.py

--- info.py
+++ info.py
@@ -4,7 +4,7 @@ err, = driver.cuInit(0)
 err, dev = driver.cuDeviceGet(0)
 pattern = re.compile(r"^CU_DEVICE_ATTRIBUTE_(.+)")
 for attr_name in dir(driver.CUdevice_attribute):
-    ma = pattern.ma(attr_name)
+    ma = pattern.match(attr_name)
     if ma:
         attr_enum = getattr(driver.CUdevice_attribute, attr_name)
         err, val = driver.cuDeviceGetAttribute(attr_enum, dev)
