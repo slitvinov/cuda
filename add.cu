d1d6e638c5f52af01feb638b20a17d3b4b4ead11
Sergey Litvinov <slitvinov@gmial.com>
2026-07-31 10:34:08 -0400

    add canonical addc

--- add.cu
+++ add.cu
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <inttypes.h>
-#include <cuda/math>
 #include "reg.h"
-enum { threads = 32, n = 40 };
-
-static __global__ void kernel(float *a, float *b, float *c) {
+enum { th = 32, n = 40 };
+static __managed__ float a[n], b[n], c[n];
+static __global__ void kernel() {
   uint64_t tid = (uint64_t)blockDim.x * blockIdx.x + threadIdx.x;
   if (tid < n)
     c[tid] = a[tid] + b[tid];
 }
 int main() {
   cudaError_t err;
-  printf("smid tid threadIdx.x blockIdx.x\n");
-  kernel<<<2, 3>>>();
+  uint64_t i;
+  for (i = 0; i < n; i++) {
+    a[i] = i;
+    b[i] = 2 * i;
+  }
+  kernel<<< (n + th - 1) / th, th>>>();
   if ((err = cudaGetLastError()) != cudaSuccess)
     goto fail;
   if ((err = cudaDeviceSynchronize()) != cudaSuccess)
-    goto fail;  
+    goto fail;
+  for (i = 0; i < n; i++)
+    printf("%.1f ", c[i]);
   return 0;
  fail:
     fprintf(stderr, "smoke: error: %s\n", cudaGetErrorString(err));
