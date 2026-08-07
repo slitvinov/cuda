333a7d6378f0b42c5324eb073ba7c97a216603dc
Sergey Litvinov <slitvinov@gmial.com>
2026-08-07 19:35:24 -0400

    B200

--- y.sh
+++ y.sh
@@ -1,7 +1,7 @@
 h=t4
 n=25
 export M_GPU
-M_GPU=T4
+M_GPU=B200
 mcu nvcc launch.cu -O2 -gencode arch=compute_100a,code=sm_100a -cudart shared -Xlinker -s -- a.out -- \
     ./a.out -n $((1<<n)) -o $h -- $h.raw $h.meta
 python launch.py $h > data/$h
