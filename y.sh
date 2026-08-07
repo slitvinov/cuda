1b89a0f4ed0535d726b8d422c753fa4196ad85ff
Sergey Litvinov <slitvinov@gmial.com>
2026-08-07 19:36:12 -0400

    use M_GPU

--- y.sh
+++ y.sh
@@ -1,7 +1,6 @@
-h=t4
 n=25
 export M_GPU
 M_GPU=B200
 mcu nvcc launch.cu -O2 -gencode arch=compute_100a,code=sm_100a -cudart shared -Xlinker -s -- a.out -- \
-    ./a.out -n $((1<<n)) -o $h -- $h.raw $h.meta
-python launch.py $h > data/$h
+    ./a.out -n $((1<<n)) -o $M_GPU -- $M_GPU.raw $M_GPU.meta
+python launch.py $M_GPU > data/$M_GPU
